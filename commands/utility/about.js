1df0de450327eeb431959ffce72287f246e29a54
hAxel <hAxel@haxel.io>
2024-12-22 19:36:45 +0000

    Converted about.js to work with discord.js v14, removed Invite, Support Server, and Patreon buttons

--- commands/utility/about.js
+++ commands/utility/about.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed, MessageActionRow, MessageButton, Message } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, Message } = require('discord.js');
 const { botInvite, supportServerInvite, githubLink, ownerTag, ownerAvatar, patreonLink} = require('../../modules/utility.js');
 const garageSchema = require('../../mongodb_schema/garageSchema.js');
 const garage = require('../garage/garage.js');
@@ -24,40 +24,40 @@ module.exports = {
         const totalVerifiedUsers = (await garageSchema.distinct("userId")).length;
         const verifiedUsersInServer = (await garageSchema.distinct("userId", { guildId: interaction.guild.id })).length;
         
-        const inviteEmbed = new MessageEmbed()
-        .setTitle('ThrottleBot Vehicle Verification')
-        .setDescription('We\'re simplifying the process of verifying your vehicles across Discord by providing a seamless and feature full experience.\nThe bot utilizes Discord\'s latest API version V9 to provide you with the latest features that are available.')
-        .addField('Features','• A garage system to store and display all your vehicles.\n• Seamless verifcation process with the help of buttons.\n• Slash commands for a powerful and interactive experience.\n• Syncing across different servers.')
-        .addField('Servers', `${totalGuilds.toLocaleString()} Servers`, true)
-        .addField('Verified Users', `${totalVerifiedUsers.toLocaleString()} Users | ${verifiedUsersInServer} Users In Server`, true)
-        .addField('Verified Rides', `${totalVerifiedRides.toLocaleString()} Vehicles | ${verifiedRidesInServer} Vehicles In Server`, true)
+        const inviteEmbed = new EmbedBuilder()
+        .setTitle('GarageBot Vehicle Verification')
+        .setDescription('We\'re simplifying the process of verifying your vehicles across Discord by providing a seamless and feature full experience.')
+        .addFields({name: 'Features', value: '• A garage system to store and display all your vehicles.\n• Seamless verifcation process with the help of buttons.\n• Slash commands for a powerful and interactive experience.', inline: false})
+        .addFields({name: 'Servers', value: `${totalGuilds.toLocaleString()} Servers`, inline: true})
+        .addFields({name: 'Verified Users', value: `${totalVerifiedUsers.toLocaleString()} Users | ${verifiedUsersInServer} Users In Server`, inline: true})
+        .addFields({name: 'Verified Rides', value: `${totalVerifiedRides.toLocaleString()} Vehicles | ${verifiedRidesInServer} Vehicles In Server`, inline: true})
         .setColor('#FFFCFF')
         .setFooter({
             text: `Made with 💖 by ${ownerTag}`,
             iconURL: ownerAvatar
         });
-
-        const InviteButton = new MessageButton()
+/*
+        const InviteButton = new ButtonBuilder()
         .setLabel('Invite')
-        .setStyle('LINK')
+        .setStyle('Link')
         .setURL(botInvite);
 
-        const supportServerButton = new MessageButton()
+        const supportServerButton = new ButtonBuilder()
         .setLabel('Support Server')
-        .setStyle('LINK')
+        .setStyle('Link')
         .setURL(supportServerInvite);
 
-        const patreonButton = new MessageButton()
+        const patreonButton = new ButtonBuilder()
         .setLabel('Patreon')
-        .setStyle('LINK')
+        .setStyle('Link')
         .setURL(patreonLink);
 
-        const row = new MessageActionRow() 
+        const row = new ActionRowBuilder() 
         .addComponents(InviteButton, supportServerButton, patreonButton)
-        
+  */      
         await interaction.reply({
             embeds: [inviteEmbed],
-            components: [row]
+  //        components: [row]
         });
 
 	},
